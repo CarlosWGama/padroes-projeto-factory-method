92a191d59c64c549c937915a6d0427c812d0c8d8
Carlos W. Gama <carloswgama@gmail.com>
2018-07-09 19:52:48 -0300

    Adicionando oque faltou comitar

--- src/exemplo/dao/UsuarioDAO.java
+++ src/exemplo/dao/UsuarioDAO.java
@@ -1,5 +1,25 @@
 package exemplo.dao;
 
+import exemplo.banco.MySQL;
+import exemplo.banco.SQLServer;
+import exemplo.banco.SQLite;
+
 public class UsuarioDAO {
 
+	private final String BANCO = "MySQL";
+	
+	public void cadastrar() {
+		String stmt = "INSERT INTO usuarios...";
+		
+		if (BANCO.equals("MySQL")) {
+			MySQL banco = new MySQL();
+			banco.query(stmt);
+		} else if (BANCO.equals("SQLServer")) {
+			SQLServer banco = new SQLServer();
+			banco.query(stmt);
+		} else if (BANCO.equals("SQLite")) {
+			SQLite banco = new SQLite();
+			banco.query(stmt);
+		}
+	}
 }
